48d135f63dcfe1602732fb2b5333b973863d62ec
DavidOngora <ongoradavid5.gmail.com>
2023-12-26 06:01:01 +0300

    removed the host

--- congnifuseApi.py
+++ congnifuseApi.py
@@ -251,4 +251,5 @@ def list_files():
     return jsonify({"files": file_list}), 200
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    # app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run()
